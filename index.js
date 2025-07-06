f21f7c7301fc2a5d80c5dc399e6ac29829b926c7
Medul Hasan <medul.cse@gmail.com>
2025-07-06 17:16:25 +0600

    downgrade rn version for map integration

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ if (__DEV__) {
   require('./ReactotronConfig');
 }
 
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
-import { Provider } from 'react-redux';
+import {name as appName} from './app.json';
+import {Provider} from 'react-redux';
 import store from './src/redux/store';
 
-const AddProvider = props => {
+const AddProvider = () => {
   return (
     <Provider store={store}>
-      <App appThemeId={props.appThemeId} />
+      <App />
     </Provider>
   );
 };
